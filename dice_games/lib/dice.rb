11a503573c4ff60f890087c603068cd635f7a76b
Malte Gergeleit <malte.gergeleit@outlook.de>
2020-05-14 16:15:00 +0200

    Dice refactored

--- dice_games/lib/dice.rb
+++ dice_games/lib/dice.rb
@@ -1,13 +1,9 @@
 # frozen_string_literal: true
 
 class Dice
+  attr_reader :values
 
   def roll(n = 1)
-      @numbers = (1..n).map{ |i| rand(5)+1 }
+      @values = (1..n).map{ |i| rand(5)+1 }
   end
-
-  def values
-      @numbers
-  end
-
 end
